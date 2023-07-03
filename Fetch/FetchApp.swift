b1d447b66f61a0997b727f71a22a67d20637b8fc
andrearamonroy <andrearamonroy@gmail.com>
2023-07-03 07:55:34 -0500

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Andrea on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
